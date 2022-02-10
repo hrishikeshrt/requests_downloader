cf54bfcd36f4c1d67bf24cdfe25dd8237edcf10d
Hrishikesh Terdalkar <hrishirt@cse.iitk.ac.in>
2022-02-10 23:15:32 +0530

    ops: change documentation theme and settings

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,24 @@ import requests_downloader
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx_rtd_theme'
+]
+
+# Autodoc settings
+autoclass_content = 'both'
+autodoc_member_order = 'bysource'
+
+# Napoleon settings
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = True
+napoleon_include_special_with_doc = True
+napoleon_use_param = True
+napoleon_use_rtype = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,7 +64,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'requests_downloader'
-copyright = "2020-2021, Hrishikesh Terdalkar"
+copyright = "2020-2022, Hrishikesh Terdalkar"
 author = "Hrishikesh Terdalkar"
 
 # The version info for the project you're documenting, acts as replacement
@@ -83,7 +100,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
@@ -129,7 +147,7 @@ latex_elements = {
 latex_documents = [
     (master_doc, 'requests_downloader.tex',
      'requests_downloader Documentation',
-     'Hrishikesh Terdalkar', 'manual'),
+     author, 'manual'),
 ]
 
 
@@ -154,9 +172,6 @@ texinfo_documents = [
      'requests_downloader Documentation',
      author,
      'requests_downloader',
-     'One line description of project.',
+     'Python package to download files',
      'Miscellaneous'),
 ]
-
-
-
