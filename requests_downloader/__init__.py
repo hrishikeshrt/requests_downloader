983f4c442b6442920d9028d4d977bb6a7872807f
Hrishikesh Terdalkar <hrishirt@cse.iitk.ac.in>
2022-04-28 13:49:36 +0530

    "build(release): bump version 0.3.1 → 0.4.0"

--- requests_downloader/__init__.py
+++ requests_downloader/__init__.py
@@ -10,7 +10,7 @@ Python package to download files
 
 __author__ = """Hrishikesh Terdalkar"""
 __email__ = "hrishikeshrt@linuxmail.org"
-__version__ = "0.3.1"
+__version__ = "0.4.0"
 
 ###############################################################################
 
